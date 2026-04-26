1e05ac501c5f928fb8399682f39b55485d4d642a
Yash Raj <yashrajbtoraskar@gmail.com>
2026-04-26 23:53:31 +0530

    Fix DB connection using Railway env variables

--- src/com/internsphere/util/DBUtil.java
+++ src/com/internsphere/util/DBUtil.java
@@ -1,15 +1,28 @@
 package com.internsphere.util;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBUtil {
-private static final String URL = "jdbc:mysql://maglev.proxy.rlwy.net:24154/railway?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-private static final String USER = "root";
-private static final String PASSWORD = "lmXDfoKnhKrOAzsNmNQvjlqdIrsgkVLp";
+    private static final String URL;
+    private static final String USER;
+    private static final String PASSWORD;
 
     static {
+        String envUrl = System.getenv("MYSQL_PUBLIC_URL");
+        if (envUrl != null && envUrl.startsWith("mysql://")) {
+            envUrl = envUrl.replace("mysql://", "");
+            String[] parts = envUrl.split("@");
+            String[] userPass = parts[0].split(":");
+            String hostDb = parts[1];
+            USER = userPass[0];
+            PASSWORD = userPass[1];
+            URL = "jdbc:mysql://" + hostDb + "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+        } else {
+            URL = "jdbc:mysql://localhost:3306/internsphere?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+            USER = "root";
+            PASSWORD = "shabnampatil";
+        }
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
