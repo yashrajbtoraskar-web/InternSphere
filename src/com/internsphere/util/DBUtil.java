14d39bc17f2f17c0bed3baa8826ac33d29fad993
Yash Raj <yashrajbtoraskar@gmail.com>
2026-04-27 00:10:18 +0530

    Hardcode Railway DB connection

--- src/com/internsphere/util/DBUtil.java
+++ src/com/internsphere/util/DBUtil.java
@@ -4,26 +4,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBUtil {
-    private static final String URL;
-    private static final String USER;
-    private static final String PASSWORD;
+    private static final String URL = "jdbc:mysql://maglev.proxy.rlwy.net:24154/railway?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASSWORD = "lmXDfoKnhKrOAzsNmNQvjlqdIrsgkVLp";
 
     static {
-        String host = System.getenv("MYSQL_HOST");
-        String port = System.getenv("MYSQL_PORT");
-        String db   = System.getenv("MYSQL_DATABASE");
-        String user = System.getenv("MYSQL_USER");
-        String pass = System.getenv("MYSQL_PASSWORD");
-
-        if (host != null) {
-            URL  = "jdbc:mysql://" + host + ":" + port + "/" + db + "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-            USER = user;
-            PASSWORD = pass;
-        } else {
-            URL  = "jdbc:mysql://localhost:3306/internsphere?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-            USER = "root";
-            PASSWORD = "shabnampatil";
-        }
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
